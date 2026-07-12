dbe85168b41860e6a5b090d19e4c7b31480421e2
Kryptx4d <166203250+Kryptx4d@users.noreply.github.com>
2026-07-12 15:22:23 +0545

    Update quick_sort_Umair.cpp

--- individual_submissions/quick_sort_Umair.cpp
+++ individual_submissions/quick_sort_Umair.cpp
@@ -14,6 +14,50 @@ int main() {
         std::cout << "Enter your choice(A-D): ";
         std::cin >> choice;
 
+        std::cout <<"\n" << std::endl;
+
+        switch (choice) {
+            case 'A':
+                std::cout << "Displaying all records....\n" << std::endl;
+                break;
+
+            case 'B':
+                std::cout << "Sorting records.....\n"<< std::endl;
+                break;
+
+            case 'C':
+                std::cout << "Searching for numbers....\n"<< std::endl;
+                break;
+
+            case 'D':
+                std::cout << "Exiting Application...\n"<< std::endl;
+                break;
+            
+            default:
+                std::cout << "Invalid Choice. Please enter letters between A-D in upper case. Try Again."<< std::endl;
+
+        }
+    } while (choice != 'D');
+
+
+
+    return 0;
+}#include <iostream>
+
+int main() {
+    char choice;
+
+    do {
+        std::cout << "\n" << std::endl;
+        std::cout << "# Sorting and Searching Interface DSA #\n";
+        std::cout << "A. Display all records: \n";
+        std::cout << "B. Sort Records: \n";
+        std::cout << "C. Search for numbers: \n";
+        std::cout << "D. Exit\n" << std::endl;
+
+        std::cout << "Enter your choice(A-D): ";
+        std::cin >> choice;
+
         switch (choice) {
             case 'A':
                 std::cout << "Displaying all records....\n" << std::endl;
