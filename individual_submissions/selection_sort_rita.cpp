261def328a8ef8a92185f24ac85617416553f53e
rita2082-0332-droid <rita2082-0332@iimscollege.edu.np>
2026-07-18 20:40:23 +0545

    Update selection_sort_rita.cpp

--- individual_submissions/selection_sort_rita.cpp
+++ individual_submissions/selection_sort_rita.cpp
@@ -8,10 +8,16 @@ using namespace chrono;
 
 long long counter = 0;
 
-// Generating  random records
+// true = show before sorting, passes and after sorting(full output)
+// false = show only testing result
+bool showDetails = true;
+
+// generates the random numbers
 void generateRecords(int data[], int size)
 {
     srand(time(NULL));
+   
+    //stores random numbers in array
 
     for (int i = 0; i < size; i++)
     {
@@ -19,34 +25,35 @@ void generateRecords(int data[], int size)
     }
 }
 
-// Displaying records
+// Display the array or record
 void displayRecords(int data[], int size)
 {
     for (int i = 0; i < size; i++)
     {
         cout << data[i] << " ";
 
+//print 10 numbers in one line
         if ((i + 1) % 10 == 0)
             cout << endl;
     }
 }
 
-// Selection Sort
+// Selection Sort function
 void selectionSort(int data[], int size)
 {
     int temp;
     int max_index;
-    int rightmost, current;
     int pass = 1;
 
-    for (rightmost = size - 1; rightmost > 0; rightmost--)
+// Repeat until the whole array is sorted
+    for (int rightmost = size - 1; rightmost > 0; rightmost--)
     {
         max_index = 0;
 
-        // Finding the largest element
-        for (current = 1; current <= rightmost; current++)
+        // Find the largest value
+        for (int current = 1; current <= rightmost; current++)
         {
-            counter++; // Comparison
+            counter++;
 
             if (data[current] > data[max_index])
             {
@@ -54,62 +61,71 @@ void selectionSort(int data[], int size)
             }
         }
 
-        // Swaping
+        // Swap if needed
         if (max_index != rightmost)
         {
             temp = data[max_index];
             data[max_index] = data[rightmost];
             data[rightmost] = temp;
 
-            counter++; // Swap
+            counter++;
         }
 
-        // Show every pass only when input size is 100
-        if (size == 100)
+        // Show passes only when showDetails is true
+        if (showDetails)
         {
             cout << "\nPass " << pass << endl;
+            cout << "Operation Counter : " << counter << endl;
 
             displayRecords(data, size);
         }
-
+ // Go to the next pass
         pass++;
     }
 }
-
+ 
 int main()
 {
-    const int SIZE = 100;   // Change to 500,1000,5000,10000,50000 to test execution time and total operations
+    // Change this value to test execution time: 100, 500, 1000, 5000, 10000, 50000 and more if project ask
+    const int SIZE = 50000;
 
     int data[SIZE];
-
+ // Generate records
     generateRecords(data, SIZE);
 
-    // Display before 
-    if (SIZE == 100)
+   // Show records before sorting
+    if (showDetails)
     {
-        cout << "========== BEFORE SORTING ==========\n\n";
+        cout << "Before Sorting\n\n";
         displayRecords(data, SIZE);
     }
 
+    // Start measuring execution time
     auto start = high_resolution_clock::now();
 
+ // Sort the records
     selectionSort(data, SIZE);
 
+     // Stop measuring execution time
     auto stop = high_resolution_clock::now();
 
-    // Display after sorting only for 100 records
-    if (SIZE == 100)
+     // Show records after sorting
+    if (showDetails)
     {
-        cout << "\n========== AFTER SORTING ==========\n\n";
+        cout << "\nAfter Sorting\n\n";
         displayRecords(data, SIZE);
     }
 
+        // Calculate execution time
     auto duration = duration_cast<milliseconds>(stop - start);
 
-    cout << "\n-----------------------------------" << endl;
-    cout << "Input Size      : " << SIZE << endl;
-    cout << "Execution Time  : " << duration.count() << " ms" << endl;
-    cout << "Total Operations: " << counter << endl;
 
+     // Display final result
+    cout << "\nInput Size       : " << SIZE << endl;
+    cout << "Execution Time   : " << duration.count() << " ms" << endl;
+    cout << "Total Operations : " << counter << endl;
+
+
+    // End of program
     return 0;
 }
