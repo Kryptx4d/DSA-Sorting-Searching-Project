b21901f4014286ae4cb8a2e28ccc996c7d7e5c4b
Kryptx4d <166203250+Kryptx4d@users.noreply.github.com>
2026-07-29 18:44:50 +0545

    Update interpolation_search_dev.cpp

--- individual_submissions/interpolation_search_dev.cpp
+++ individual_submissions/interpolation_search_dev.cpp
@@ -1,1 +1,19 @@
+Run time measurement : #include <chrono>
+using namespace std::chrono;
 
+// Starting the timer before the search
+auto startTime = high_resolution_clock::now();
+
+// Running the interpolation search
+int foundPos = interpolationSearch(arr, n, target, steps);
+
+// Stopping the timer after search completes
+auto endTime = high_resolution_clock::now();
+
+// Getting the time difference and converting to milliseconds
+auto duration = duration_cast<microseconds>(endTime - startTime);
+double timeMs = duration.count() / 1000.0;
+
+// Displaying the results
+cout << "Time taken: " << timeMs << " ms" << endl;
+cout << "Steps performed: " << steps << endl;
